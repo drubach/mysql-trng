f7f23111a734ecd9de8536fc7c79d38b33484f04
drubach <daniel.c.rubach@gmail.com>
2021-10-31 19:03:47 -0400

    connect to db in Python

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,4 +1,21 @@
 import os 
 import pymysql
 
-username = os.getenv("${env:USERNAME}")
+username = "drubach"
+
+# Connect to database
+connection = pymysql.connect(host='LocalHost',
+                            user = username,
+                            password = '!RmrEJJRy1258',
+                            db = 'Chinook')
+
+try:
+    #Run a query
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM Artist;"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(result)
+finally:
+    #Close the connection, regardless of whther the above was successful.
+    connection.close()
